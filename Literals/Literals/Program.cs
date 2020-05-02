55439d29cb75a232ebe41e15a6401e967afb813d
CARICOM12 <0190a@ukr.net>
2020-05-02 21:56:58 +0300

    Add files via upload

---
+++ Literals/Literals/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Literals
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            long literalOfTypeLong 134556L;
+            ulong literalOfTypeULong 2394UL;
+            float literalOfTypeFloat 234.2334f;
+            double literalOfTypeDouble 220.999D;
+            decimal literalOfTypeDecimal 222.99999M;
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
